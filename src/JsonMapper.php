ab1ac2d304c8f2b11a3c4fa65c8995ada613aecd
TS <tswienty@betasoft.pl>
2023-06-30 11:57:38 +0200

    d

--- src/JsonMapper.php
+++ src/JsonMapper.php
@@ -9,4 +9,45 @@
  */
 final class JsonMapper {
 
+
+    protected $logger;
+    public function setLogger($logger) {
+
+        $this->logger = $logger;
+
+    }
+
+
+    protected function log($level, $message, array $context = array()) {
+
+        if ($this->logger) {
+            $this->logger->log($level, $message, $context);
+        }
+
+    }
+
+
+    public function map($json, $object) {
+
+        if (!is_object($json)) {
+            throw new InvalidArgumentException(
+                'JsonMapper::map() requires first argument to be an object'
+                . ', ' . gettype($json) . ' given.'
+            );
+        }
+
+        if (!is_object($object)) {
+            throw new InvalidArgumentException(
+                'JsonMapper::map() requires second argument to be an object'
+                . ', ' . gettype($object) . ' given.'
+            );
+        }
+
+    }
+
+
+    public function mapArray($json, $array, $class = null, $parent_key = '') {
+        
+    }
+
 }
